19d2b53ea33a125d61c3da0adca59584fb3f9828
tinch08 <62726897+tinch08@users.noreply.github.com>
2020-03-27 00:37:18 -0300

    Cambio de Fw en Izaje

--- src/crane_constants.m
+++ src/crane_constants.m
@@ -1,8 +1,6 @@
-
 clc;
 clear;
 close all;
-%lo hice yo
 %% Traslacion de Carro
 Rw = 0.5;%Radio Primitivo Rueda [m]
 i_c = 15;%Relacion de Transmision "Caja Reductora"
@@ -28,19 +26,35 @@ bcx = 1000;%Friccion Horizontal
 
 %% Masas
 Mc = 50000;%Masa de Carro
-Mh = 5000;%Masa de la Carga
+<<<<<<< HEAD
+Mh = 15000;%Masa de la Carga
 %Gancho Vacio ml=15000
 %Nominal 65.000
 %Minima 17.000
 Mt = Mc+Mh;%Masa Total
 
+=======
+%Gancho vacío 
+M_h =15000.0; %[kg] masa gancho
+% Contenedor
+M_cn = 50000; %[kg] masa contenedor nominal
+M_cmin = 2000; %[kg] masa contenedor vacio
+% y_c = 2.5; %[m] altura 2.5m bajo gancho
+%Gancho vacio
+M_l0 = M_h;
+%Gancho con carga nominal
+M_ln = M_h + M_cn; %[kg] m l =65000 kg (15000 kg + 50000 kg)
+%Gancho con carga minima
+M_lmin = M_h + M_cmin; %[kg] m l =17000 kg (15000 kg + 2000 kg)
+%Intermedia (contenedor cargado con carga menor que nominal)
+>>>>>>> 2c4a9c5833fa29018bafdeb510d31844b3cec14b
 %% Gravedad
 g = 9.80665;%[m/s2]
 
 %% Equivalentes
-Jeq_c = (Mt*(Rw^2)/(i_c^2))+(Jw/(i_c^2))+Jm_c;
+Jeq_c = (Mc*(Rw^2)/(i_c^2))+(Jw/(i_c^2))+Jm_c;
 beq_c = beq_c;
-Jeq_i = (-Mh*Rd^2/(i_i^2))+(Jd/i_i^2)+Jm_i;
+Jeq_i = (-M_ln*Rd^2/(i_i^2))+(Jd/i_i^2)+Jm_i;
 beq_i = beq_i;
 
 %% Condiciones Iniciales
@@ -70,6 +84,26 @@ n_c = 2.5;
 wpos_c = wn_c*4;
 wv_c = n_c * wpos_c;
 wi_c = wpos_c / n_c;
-ba_c = Jeq_c * wv_c
-ksa_c = ba_c * wpos_c
-kisa_c = ksa_c * wi_c
+ba_c = Jeq_c * wv_c;
+ksa_c = ba_c * wpos_c;
+kisa_c = ksa_c * wi_c;
+
+%% Torque maximo izaje
+amax_i = 1; %[m/s^2]
+Fmax_i = M_ln * amax_i; 
+Tmax_i = Fmax_i * Rd / i_i;
+%% Polos sistema Carro
+Ai = [0 1;0 -beq_i/Jeq_i];
+pi = eig(Ai);
+syms s;
+(s-pi(1))*(s-pi(2));
+%% Sintonia Serie PID Carro
+wn_i = abs(pi(2));
+n_i = 2.5;
+wpos_i = wn_i*4;
+wv_i = n_i * wpos_i;
+wi_i = wpos_i / n_i;
+ba_i = Jeq_i * wv_i;
+ksa_i = ba_i * wpos_i;
+kisa_i = ksa_i * wi_i;
+
